cb80ae1536b31ac59379252b274eec525dac493f
blvkroot <hsalukamba17@gmail.com>
2021-07-12 02:03:22 +0100

    Player container finished.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
-import { Header } from '../components/Header';
 import '../styles/global.scss';
+
+import { Header } from '../components/Header';
+import { Player } from '../components/Player';
+
+import appStyles from '../styles/app.module.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <Header />
-      <Component {...pageProps} />
+    <div className={appStyles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
     </div>
   )
 }
